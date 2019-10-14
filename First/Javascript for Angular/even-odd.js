ac9bfce9256a22abd017caafb502a3f904fa461a
PratikshaDhumal96 <pratikshadhumal2019@gmail.com>
2019-10-14 18:34:44 +0530

    even-odd programe in javascript

--- First/Javascript for Angular/even-odd.js
+++ First/Javascript for Angular/even-odd.js
@@ -1,11 +1,11 @@
-function even_odd(num){
-    if(num%2==0)
-    {
-        console.log("number is even");
-    }
-    else{
-        console.log("number is odd");
-    }
-    }
-    even_odd(10);
-    even_odd(3);
+function checkEven(val){
+    return (val%2 == 0);
+}
+
+val = 3;
+
+if(checkEven(val)){
+   alert("Number is even");
+} else {
+   alert("Number is odd");
+}
