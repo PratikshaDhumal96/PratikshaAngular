8bd516ef7a2f596350f1f7c3fac79fe17668add2
PratikshaDhumal96 <pratikshadhumal2019@gmail.com>
2019-10-23 13:32:07 +0530

    Manipulate Arrays with Unshift(). unshift() is use To add the element in the beginning of the Array

---
+++ javascript/ArrayWithUnshift.js
@@ -0,0 +1,14 @@
+//unshift() use to add the element in the beginning of the array
+
+var OurArray=["Pratiksha",3,4];
+ OurArray.shift();//it is now [3,4]
+ OurArray.unshift("Dhumal");
+ console.log(OurArray);
+
+
+ var myArray=[[1,2,3],["p","q","cat"]];
+  myArray.shift();//it is now [[]]
+console.log(myArray);
+myArray.unshift(["prat",3,6]);
+console.log(myArray);
+ 
