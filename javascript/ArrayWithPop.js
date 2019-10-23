303a7e8b4cb0f11bc4271106a0b96783da6166a5
PratikshaDhumal96 <pratikshadhumal2019@gmail.com>
2019-10-23 12:42:38 +0530

    Manipulate arrays with Push()

---
+++ javascript/ArrayWithPop.js
@@ -0,0 +1,13 @@
+// we can remove an item from an array with pop()
+
+var OurArray=[1,2,3];
+var RemovedFromOurArray=OurArray.pop();
+
+//RemovedFromOurArray is now equals 3,and ourArray now equals[1,2]
+
+console.log(OurArray);
+
+
+var MyArray=[3,4,5];
+var RemovedFromMyArray=MyArray.pop();
+ console.log(MyArray);
