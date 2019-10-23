a554b6c2ced96ea513e18bb93b173ef614294854
PratikshaDhumal96 <pratikshadhumal2019@gmail.com>
2019-10-23 12:17:10 +0530

    Manipulate Arrays with push()

---
+++ javascript/ArrayWithPush.js
@@ -0,0 +1,9 @@
+var MyArray=[["Pratiksha","e","cat"]];
+MyArray.push(["happy","joy"]);
+
+console.log(MyArray);
+
+var OurArray=[["Deshmukh",23],["Dhumal",30]];
+OurArray.push(["Mango",23]);
+
+console.log(OurArray);
