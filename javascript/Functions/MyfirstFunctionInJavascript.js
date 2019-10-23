cafd0ea475ea76c6bd6dbfa4805ab816dd78feb0
PratikshaDhumal96 <pratikshadhumal2019@gmail.com>
2019-10-23 14:09:40 +0530

    How To Create function in javascript. It allow us to create reusable code in javascript.

---
+++ javascript/Functions/MyfirstFunctionInJavascript.js
@@ -0,0 +1,16 @@
+
+// function allow us to create reusable code in javascript.
+
+function OurReusableFunction(){
+    console.log("Hello,World");
+}
+
+OurReusableFunction();
+
+
+
+function reusableFunction(){
+    console.log("Hey,World!!!");
+}
+
+reusableFunction();
