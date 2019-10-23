7796d5655d2a3a523bc9ee32544a14f7d8760f4c
PratikshaDhumal96 <pratikshadhumal2019@gmail.com>
2019-10-23 13:07:28 +0530

    Manipulate Arrays with Shift()

---
+++ javascript/ArrayWithShift.js
@@ -0,0 +1,10 @@
+var OurArray=["Pratiksha","p",["cat"]];
+ var removedFromOurArray=OurArray.shift();
+ //removedFromOurArray now equals "Pratiksha" And OurArray now equals ["p",["cat"]] 
+
+ console.log(OurArray);
+
+ var myArray=[["prati",3],["Dog",34,"shivajinagar"]];
+  var RemovedFrommyArray=myArray.shift();
+
+  console.log(myArray);
