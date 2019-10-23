ce67c98f1d45f71395bd4a4e01686c61542284b8
PratikshaDhumal96 <pratikshadhumal2019@gmail.com>
2019-10-23 17:47:59 +0530

    Use of Return Statement. You can return a value from the function with that return statement.

---
+++ javascript/Functions/return.js
@@ -0,0 +1,13 @@
+// You can return a value from the function with that return statement.
+
+function returnSum(num){
+    return num-7;
+}
+
+console.log(returnSum(10));
+
+function timesFive(num){
+    return num*5;
+}
+
+console.log(timesFive(2));
