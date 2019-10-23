b35fb8c725ce51f19f6b24720e72081e8369fe78
PratikshaDhumal96 <pratikshadhumal2019@gmail.com>
2019-10-23 15:47:52 +0530

    Passing values to functions with Arguments

---
+++ javascript/Functions/FunctionWithArgument.ts
@@ -0,0 +1,12 @@
+function OurFunctionWithArgs(a,b){
+    console.log(a-b);
+}
+ OurFunctionWithArgs(10,5);
+
+
+
+ function functionWithArgs(a,b){
+     console.log(a*b);
+ }
+
+ functionWithArgs(20,3);
--- javascript/Functions/MyfirstFunctionInJavascript.js
+++ javascript/Functions/MyfirstFunctionInJavascript.js
@@ -14,3 +14,4 @@ function reusableFunction(){
 }
 
 reusableFunction();
+ 
