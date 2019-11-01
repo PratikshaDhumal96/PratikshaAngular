b023cacdaf570223c563bc4c01dd50fd495cf506
PratikshaDhumal96 <pratikshadhumal2019@gmail.com>
2019-11-01 14:46:13 +0530

    Returning boolean values from func= All comparison operators returns booleans true of false values.

---
+++ javascript/UseOfReturn/ReturningBooleanValuesFromFunctions.js
@@ -0,0 +1,21 @@
+// function isLess(a,b){
+//     if (a<b){
+//         return true;
+//     }
+//     else{
+//         return false;
+//     }
+
+// }
+
+// console.log(isLess(10,15));
+
+
+//We need to Remember that all comparison operators returns booleans true of false values.
+
+
+function isLess(a,b){
+    return a<b
+}
+
+console.log(isLess(10,15));
