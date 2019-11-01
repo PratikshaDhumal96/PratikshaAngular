23e7c86ede10cb7fa01595c687727e2031ae99b4
PratikshaDhumal96 <pratikshadhumal2019@gmail.com>
2019-11-01 19:22:18 +0530

    Access object properties with dot notation

---
+++ javascript/Object/AccessingObjectPropertiesWithDotNotation.js
@@ -0,0 +1,13 @@
+var testObj={
+        "hat":"ballcap",
+        "shirt":"jersey",
+        "shoes":"cleats",
+};
+
+var hatValue=testObj.hat;
+var shirtValue=testObj.shirt;
+var shoesValue=testObj.shoes;
+
+console.log(hatValue);//ballcap
+console.log(shirtValue);//jersey
+console.log(shoesValue);//cleats
