c7764fe1b4b7bcad696b52d10eef8b27a2820ad0
PratikshaDhumal96 <pratikshadhumal2019@gmail.com>
2019-11-01 14:22:57 +0530

    Replacing If Else With switch Case Statement. It is easy to write code without repeating a one action. Or Without Provided to repeated value.

---
+++ javascript/SwitchStatements/Replacing_if_else_with_SwitchCaseStatement.js
@@ -0,0 +1,30 @@
+function ReplacingIfElseWithSwitchCaseStatement(val){
+    var answer="";
+
+    switch(val){
+case "bob":
+    answer="Marley";
+    break;
+case 42:
+    answer="the answer";
+    break;
+case 1:
+    answer="their is no #1";
+    break;
+case 99:
+    answer="Missed me by this much";
+    break;
+case 7:
+    answer="Ate Nine";
+    break;
+}
+
+/*
+else if( val === 7){
+    answer="Ate Nine";*/
+
+
+    return answer;
+}
+
+console.log(ReplacingIfElseWithSwitchCaseStatement(42));
