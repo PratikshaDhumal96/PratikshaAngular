245177eff2e24e5366674cdd4cee8194f59325ef
PratikshaDhumal96 <pratikshadhumal2019@gmail.com>
2019-11-04 15:16:30 +0530

    How to find out even & Odd Number Using for loop

---
+++ javascript/Loops/EvenOddNumberUsingForLoop.js
@@ -0,0 +1,21 @@
+
+// To display even Number.
+var OurArray=[];
+
+for(var i=0; i<10; i+=2){
+    OurArray.push(i);
+}
+
+console.log(OurArray);
+
+
+// To Display Odd Number.
+
+var MyArray=[];
+
+for(var i=1; i<10; i+=2){
+    MyArray.push(i);
+}
+
+
+console.log(MyArray);
