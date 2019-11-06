bf121580dd4ff81914aa1347748f4db6c2b15543
PratikshaDhumal96 <pratikshadhumal2019@gmail.com>
2019-11-06 13:09:36 +0530

    Use Of ParseInt Function

---
+++ javascript/ParseInt.js
@@ -0,0 +1,15 @@
+function  converToInteger(str){
+
+    return parseInt(str);
+}
+
+console.log(converToInteger("56"));
+
+
+
+
+// function convertToInteger(str){
+//     return parseInt(str);
+// }
+
+// console.log(convertToInteger("56"));
