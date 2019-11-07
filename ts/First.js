ed0552ee736700d7c49f514330c915a36090dfa1
PratikshaDhumal96 <pratikshadhumal2019@gmail.com>
2019-11-07 14:35:44 +0530

    can't understand ask

---
+++ ts/First.js
@@ -0,0 +1,8 @@
+var custName = "Android";
+var custMob = 12345;
+// custMob='' It gives u error bcoz u explicitely define the type of cutMob is number in above  And here u r assign the string value.
+// tyescript is a typed superset of javascript that compiles to plain javascript.
+// remember that we define a type after a semicolan(:)
+function dial(num) {
+    return " \n" + num * num + ":stsfyfsqy\nsfasgs:sfhaa\nbasbs:askhs\n";
+}
---
+++ ts/classes.ts
@@ -0,0 +1,26 @@
+
+
+class Car{
+
+}
+
+class Bmw extends Car{
+
+}
+
+let cr : Car = new Car()
+
+
+interface GpsListener {
+    locate()
+}
+
+class Device implements GpsListener{
+    locate(){
+
+    }
+
+}
+
+let dv = new Device()
+dv.locate()
