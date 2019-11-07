3fbbabfe27ca4d0b0f5ebf0281202724c32f9796
PratikshaDhumal96 <pratikshadhumal2019@gmail.com>
2019-11-07 17:50:51 +0530

    shapes

---
+++ ts/comp-dep.ts
@@ -0,0 +1,30 @@
+
+
+
+export class RAM{
+
+}
+export class display{
+
+}
+export class VGA{
+
+}
+export class PCI{
+
+}
+export class CMOS {
+
+}
+export class ALU{
+
+}
+export class CPU {
+
+    ghz : number
+
+    Processing(){
+        
+    }
+
+}
