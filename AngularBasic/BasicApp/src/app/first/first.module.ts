b6cdc641ce5adfd2f48afca8e7af971b3e70bd32
PratikshaDhumal1612 <pratikshadhumal2019@gmail.com>
2019-11-14 03:17:29 -0800

    DemoProject

---
+++ AngularBasic/BasicApp/src/app/first/first.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FirstSampleComponent } from '../first-sample/first-sample.component';
+
+
+
+@NgModule({
+  declarations: [FirstSampleComponent],
+  imports: [CommonModule],
+  exports: [FirstSampleComponent]
+})
+export class FirstModule { }
