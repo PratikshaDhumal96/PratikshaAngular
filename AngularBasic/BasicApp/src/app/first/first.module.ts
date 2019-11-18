cc13bf2f3da89880fa776d2a8d4e5444fcdf75f4
PratikshaDhumal1612 <pratikshadhumal2019@gmail.com>
2019-11-18 15:51:16 +0530

    components:created components and integrated bootstrap4

--- AngularBasic/BasicApp/src/app/first/first.module.ts
+++ AngularBasic/BasicApp/src/app/first/first.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FirstSampleComponent } from '../first-sample/first-sample.component';
+import { MyCardComponent } from './my-card/my-card.component';
 
 
 
 @NgModule({
-  declarations: [FirstSampleComponent],
+  declarations: [FirstSampleComponent, MyCardComponent],
   imports: [CommonModule],
-  exports: [FirstSampleComponent]
+  exports: [FirstSampleComponent,MyCardComponent]
 })
 export class FirstModule { }
