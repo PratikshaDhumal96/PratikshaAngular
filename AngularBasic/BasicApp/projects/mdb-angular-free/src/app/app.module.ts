f1244711a8e674e7630e3fddd18b195e85154340
PratikshaDhumal1612 <pratikshadhumal2019@gmail.com>
2019-11-28 15:32:01 +0530

    create a calculator using mdb

---
+++ AngularBasic/BasicApp/projects/mdb-angular-free/src/app/app.module.ts
@@ -0,0 +1,26 @@
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppComponent } from './app.component';
+
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { FormsModule } from '@angular/forms';
+import { CalculatorComponent } from './calculator/calculator.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CalculatorComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MDBBootstrapModule.forRoot(),
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
